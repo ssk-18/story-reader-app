4567850788dfd524a09c25e5aee6b21458bd17c2
ssk-18 <24505a1213@pvpsit.ac.in>
2026-03-12 00:04:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,42 +3,53 @@ const stories = [
 {
 title:"The Haunted House",
 category:"horror",
-content:"One night, Rahul entered an old haunted house. Suddenly he heard footsteps behind him..."
+content:"One night Rahul entered an abandoned house. Suddenly the door slammed..."
 },
 
 {
 title:"Love in the Rain",
 category:"romance",
-content:"Ananya met Arjun on a rainy evening. Their story began with a simple umbrella..."
+content:"Ananya met Arjun during a rainy evening and their journey started..."
 },
 
 {
 title:"Journey to Mars",
 category:"scifi",
-content:"In the year 2050 humans finally landed on Mars to build their first colony..."
+content:"In 2050 humans finally built a colony on Mars..."
 },
 
 {
 title:"Never Give Up",
 category:"motivation",
-content:"Success doesn't come overnight. Hard work and patience lead to greatness..."
+content:"Success belongs to those who never stop trying."
 }
 
 ];
 
+let currentStories = stories;
+
 function displayStories(list){
 
-const container = document.getElementById("storyContainer");
+const container=document.getElementById("storyContainer");
+
 container.innerHTML="";
 
-list.forEach(story => {
+list.forEach(story=>{
 
-const div = document.createElement("div");
+const div=document.createElement("div");
+
 div.className="story";
 
-div.innerHTML = `
+div.innerHTML=`
+
 <h3>${story.title}</h3>
+
 <p>${story.content}</p>
+
+<button class="bookmark" onclick="bookmarkStory('${story.title}')">
+Bookmark
+</button>
+
 `;
 
 container.appendChild(div);
@@ -47,19 +58,60 @@ container.appendChild(div);
 
 }
 
+
 function filterStories(category){
 
 if(category==="all"){
-displayStories(stories);
+
+currentStories=stories;
+
 }
+
 else{
 
-const filtered = stories.filter(s => s.category===category);
+currentStories=stories.filter(s=>s.category===category);
+
+}
+
+displayStories(currentStories);
+
+}
+
+
+function searchStories(){
+
+const keyword=document.getElementById("searchBar").value.toLowerCase();
+
+const filtered=currentStories.filter(story =>
+story.title.toLowerCase().includes(keyword)
+);
 
 displayStories(filtered);
 
 }
 
+
+function bookmarkStory(title){
+
+let bookmarks=JSON.parse(localStorage.getItem("bookmarks"))||[];
+
+if(!bookmarks.includes(title)){
+
+bookmarks.push(title);
+
+localStorage.setItem("bookmarks",JSON.stringify(bookmarks));
+
+alert("Story Bookmarked!");
+
 }
+
+}
+
+
+document.getElementById("darkToggle").onclick=function(){
+
+document.body.classList.toggle("dark");
+
+};
 
 displayStories(stories);
